9667a272a9c05a0903c7d48870a3db535f145823
Gulyapas Poonkawinsiri <gulyapas.p@gmail.com>
2022-06-10 16:40:14 +0700

    move route in nested route into app.js file and use Outlet component to point where the component should be placed

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,16 +1,11 @@
-import { Routes, Route, Link } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 const Welcome = () => {
   return (
     <section>
       <h1>The Welcome Page</h1>
-      <Link to="new-user" >New User</Link>
-      <Routes>
-        <Route
-          path="new-user"
-          element={<p>Welcome, new user!</p>}
-        ></Route>
-      </Routes>
+      <Link to="new-user">New User</Link>
+      <Outlet />
     </section>
   );
 };
