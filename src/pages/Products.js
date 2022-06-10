4ff88c1a1fa7f4c71d41064db7419110d3c66b20
Gulyapas Poonkawinsiri <gulyapas.p@gmail.com>
2022-06-10 16:48:47 +0700

    useHistory and Prompt component is no longer exist in v6, for useHistory - use useNavigation instead, for Prompt component - custom component need to be created

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,18 +1,26 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
 
 const Products = () => {
+  // in v6, use in useEffect, or after finish http request
+  // const navigate = useNavigate();
+  // navigate("/welcome"); // push
+  // navigate("/welcome", { replace: true }); // replace
+  // navigate(-1); // go to previous page
+  // navigate(-2); // go to the page before previous page
+
   return (
     <section>
       <h1>The Products Page</h1>
       <ul>
         <li>
-          <Link to='/products/p1'>A Book</Link>
+          <Link to="/products/p1">A Book</Link>
         </li>
         <li>
-          <Link to='/products/p2'>A Carpet</Link>
+          <Link to="/products/p2">A Carpet</Link>
         </li>
         <li>
-          <Link to='/products/p3'>An Online Course</Link>
+          <Link to="/products/p3">An Online Course</Link>
         </li>
       </ul>
     </section>
